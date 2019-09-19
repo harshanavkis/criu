9569044cc5fdb8a633c39cba82a1566f97754712
Harshavardhan Unnibhavi <hvubfoss@gmail.com>
2019-09-19 22:55:06 +0530

    crit: Anonymize unix socket names.
    The unix socket names are replaced by their corresponding sha1 hash
    values. The top levels names are kept the same.
    
    Resolve Issue #360
    
    Signed-off-by: Harshavardhan Unnibhavi <hvubfoss@gmail.com>

--- lib/py/anonymize.py
+++ lib/py/anonymize.py
@@ -6,70 +6,112 @@
 #     - encode the json dict back to a binary image, which is now anonymized
 
 # The following contents are being anonymized:
-#     - Paths to files
+#     - Paths to files(unix files and regular files)
 #     - All memory contents are just removed
 #     - All process names
 
 import hashlib
 import os
+import copy
+
+
+def file_name_anon(file_list, ftype):
+    levels = {}
+    abs_ns_usk = []
+
+    checksum = hashlib.sha1()
+
+    for f in file_list:
+        f_path = f
+        if len(f) > 0 and f[0] == '@':
+            abs_ns_usk.append(True)
+            f_path = f_path[1:]
+        else:
+            abs_ns_usk.append(False)
+
+        f_path = f_path.split('/')
+        lev_num = 0
+
+        for i, p in enumerate(f_path):
+            if p == '':
+                continue
+            if lev_num not in levels:
+                levels[lev_num] = {}
+            if p not in levels[lev_num]:
+                if i == 1:
+                    levels[lev_num][p] = p
+                else:
+                    checksum.update(p)
+                    levels[lev_num][p] = checksum.hexdigest()
+            lev_num += 1
+
+    for i, f in enumerate(file_list):
+        f_path = f
+
+        if f_path == '/':
+            continue
+
+        if len(f) > 0 and f[0] == '@':
+            f_path = f[1:]
+
+        f_path = f_path.split('/')
+        lev_num = 0
+
+        for j, p in enumerate(f_path):
+            if p == '':
+                continue
+            f_path[j] = levels[lev_num][p]
+            lev_num += 1
+        f_path = '/'.join(f_path)
+
+        if abs_ns_usk[i]:
+            f_path = '@'+f_path
+
+        file_list[i] = f_path
+
+    return file_list
 
 
 def files_anon(image):
-    levels = {}
-
-    fname_key = 'reg'
-    checksum = hashlib.sha1()
+    file_types = {}
+    file_anons = {}
+    type_names = {'REG': 'reg', 'UNIXSK': 'usk'}
 
     for e in image['entries']:
-        if fname_key in e:
-            f_path = e[fname_key]['name']
+        if e['type'] in type_names:
+            tname = type_names[e['type']]
+            if tname not in file_types:
+                file_types[tname] = []
+            file_types[tname].append(e[tname]['name'])
 
-            f_path = f_path.split('/')
-            lev_num = 0
+    temp_file_types = copy.deepcopy(file_types)
 
-            for i, p in enumerate(f_path):
-                if p == '':
-                    continue
-                if lev_num not in levels:
-                    levels[lev_num] = {}
-                if p not in levels[lev_num]:
-                    if i == 1:
-                        levels[lev_num][p] = p
-                    else:
-                        checksum.update(p)
-                        levels[lev_num][p] = checksum.hexdigest()
-                lev_num += 1
+    for ft in file_types:
+        file_anons[ft] = file_name_anon(temp_file_types[ft], ft)
+        file_anons[ft] = dict(zip(file_types[ft], file_anons[ft]))
 
     for i, e in enumerate(image['entries']):
-        if fname_key in e:
-            f_path = e[fname_key]['name']
-
-            if f_path == '/':
-                continue
-
-            f_path = f_path.split('/')
-            lev_num = 0
-
-            for j, p in enumerate(f_path):
-                if p == '':
-                    continue
-                f_path[j] = levels[lev_num][p]
-                lev_num += 1
-            f_path = '/'.join(f_path)
-            image['entries'][i][fname_key]['name'] = f_path
+        if e['type'] in type_names:
+            tname = type_names[e['type']]
+            fname = e[tname]['name']
+            if e['type'] in type_names:
+                image['entries'][i][tname]['name'] = file_anons[tname][fname]
 
     return image
 
+
 def page_anon(image):
-    page_num  = image['entries'][0]['pages_id']
+    page_num = image['entries'][0]['pages_id']
     page_file = 'pages-{}.img'.format(page_num)
     if os.stat(page_file).st_size > 0:
         page_size = os.stat(page_file).st_size
         image['entries'][0]['page_size'] = page_size
     else:
-        print("Could not find page corresponding to page id:{}".format(pages_id))
+        print("Could not find page corresponding to page id:{}".format(
+            page_num))
 
     return image
+
 
 def core_anon(image):
     regs = image['entries'][0]['thread_info']['gpregs']
@@ -94,6 +136,7 @@ def core_anon(image):
     image['entries'][0]['thread_core']['comm'] = proc_names[thread_core]
 
     return image
+
 
 anonymizers = {
     'FILES': files_anon,
