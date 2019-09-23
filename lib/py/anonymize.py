ea74ae6bec81660e14e1448f0ddcb00964bcb552
Harshavardhan Unnibhavi <hvubfoss@gmail.com>
2019-09-23 11:40:55 +0530

    crit: Anonymize stream data
    The following stream's contents are anonymized:
      - tcp stream
      - fifo
      - pipe
      - queue
    
    The contents are just a randomly shuffled version of the original stream
    contents.
    
    Resolve Issue #360.
    
    Signed-off-by: Harshavardhan Unnibhavi <hvubfoss@gmail.com>

--- lib/py/anonymize.py
+++ lib/py/anonymize.py
@@ -11,11 +11,13 @@
 #     - All process names
 #     - Registers
 #     - IP addresses
+#     - stream data(fifo, pipe, queue, tcp)
 
 import hashlib
 import os
 import copy
 import socket, struct
+import random
 
 
 def file_name_anon(file_list, ftype):
@@ -152,10 +154,44 @@ def core_anon(image):
     return image
 
 
+def shuffle_stream(stream_data):
+    if len(stream_data) == 0:
+        return stream_data
+
+    stream_data = stream_data.replace('\n', '').replace('=', '')
+    stream_data = ''.join(random.sample(stream_data, len(stream_data)))
+    if len(stream_data)%4 != 0:
+        pad = len(stream_data)%4
+        stream_data += '='*pad
+
+    return stream_data
+
+
+def stream_anon(image):
+    for i, e in enumerate(image['entries']):
+        image['entries'][i]['extra'] = shuffle_stream(e['extra'])
+
+    return image
+
+
+def tcp_stream_anon(image):
+    for i, e in enumerate(image['entries']):
+        image['entries'][i]['extra']['outq'] = shuffle_stream(
+            e['extra']['outq'])
+        image['entries'][i]['extra']['inq'] = shuffle_stream(
+            e['extra']['inq'])
+
+    return image
+
+
 anonymizers = {
     'FILES': files_anon,
     'PAGEMAP': page_anon,
-    'CORE': core_anon
+    'CORE': core_anon,
+    'PIPES_DATA': stream_anon,
+    'FIFO_DATA': stream_anon,
+    'SK_QUEUES': stream_anon,
+    'TCP_STREAM': tcp_stream_anon
 }
 
 
